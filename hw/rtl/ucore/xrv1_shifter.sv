d4addb3fd3e03db350cde2aefa5b70530acbabef
Alexey Baturo <baturo.alexey@gmail.com>
2024-12-18 13:51:52 +0300

    Fix shifter to properly handle arithmetic right shifts

--- hw/rtl/ucore/xrv1_shifter.sv
+++ hw/rtl/ucore/xrv1_shifter.sv
@@ -16,43 +16,49 @@ module xrv1_shifter
     ////////////////////////////////////////////////////////////////////////////////
     logic [DATA_WIDTH_P-1:0] shiftx8, shiftx2, shiftx1;
     logic [DATA_WIDTH_P-1:0] rev_data_r;
+
+    logic shifting_bit;
+    assign shifting_bit = (lshift_i) ? 0 :
+                          (arith_i)  ? data_i[DATA_WIDTH_P-1] :
+                                       0;
+
     ////////////////////////////////////////////////////////////////////////////////
     always_comb begin
         foreach (data_i[i])
-            rev_data_r[i] = data_i[31-i];
+            rev_data_r[i] = data_i[DATA_WIDTH_P-1-i];
     end
     ////////////////////////////////////////////////////////////////////////////////
     always_comb begin//2
         case ({lshift_i, shamt_i[0]})
-            0: shiftx1 = data_i[31:0];
-            1: shiftx1 = {{1{arith_i}},data_i[31:1]};
-            2: shiftx1 = rev_data_r[31:0];
-            3: shiftx1 = {{1{arith_i}}, rev_data_r[31:1]};
+            0: shiftx1 = data_i[DATA_WIDTH_P-1:0];
+            1: shiftx1 = {{1{shifting_bit}},data_i[DATA_WIDTH_P-1:1]};
+            2: shiftx1 = rev_data_r[DATA_WIDTH_P-1:0];
+            3: shiftx1 = {{1{shifting_bit}}, rev_data_r[DATA_WIDTH_P-1:1]};
         endcase
     end
     ////////////////////////////////////////////////////////////////////////////////
     always_comb begin//2
         case (shamt_i[2:1])
-            0: shiftx2 = shiftx1[31:0];
-            1: shiftx2 = {{2{arith_i}},shiftx1[31:2]};
-            2: shiftx2 = {{4{arith_i}},shiftx1[31:4]};
-            3: shiftx2 = {{6{arith_i}},shiftx1[31:6]};
+            0: shiftx2 = shiftx1[DATA_WIDTH_P-1:0];
+            1: shiftx2 = {{2{shifting_bit}},shiftx1[DATA_WIDTH_P-1:2]};
+            2: shiftx2 = {{4{shifting_bit}},shiftx1[DATA_WIDTH_P-1:4]};
+            3: shiftx2 = {{6{shifting_bit}},shiftx1[DATA_WIDTH_P-1:6]};
         endcase
     end
     ////////////////////////////////////////////////////////////////////////////////
     always_comb begin//8
         case (shamt_i[4:3])
-            0: shiftx8 = shiftx2[31:0];
-            1: shiftx8 = {{8{arith_i}},shiftx2[31:8]};
-            2: shiftx8 = {{16{arith_i}},shiftx2[31:16]};
-            3: shiftx8 = {{24{arith_i}},shiftx2[31:24]};
+            0: shiftx8 = shiftx2[DATA_WIDTH_P-1:0];
+            1: shiftx8 = {{8{shifting_bit}},shiftx2[DATA_WIDTH_P-1:8]};
+            2: shiftx8 = {{16{shifting_bit}},shiftx2[DATA_WIDTH_P-1:16]};
+            3: shiftx8 = {{24{shifting_bit}},shiftx2[DATA_WIDTH_P-1:24]};
         endcase
     end
     assign rshf_data_o = shiftx8;
     ////////////////////////////////////////////////////////////////////////////////
     always_comb begin
         foreach (data_i[i])
-            lshf_data_o[i] = shiftx8[31-i];
+            lshf_data_o[i] = shiftx8[DATA_WIDTH_P-1-i];
     end
     ////////////////////////////////////////////////////////////////////////////////
 
